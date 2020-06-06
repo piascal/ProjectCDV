3ece02e01f3b5c6682ddda7237fe9c431dfd4985
Aleksandra <aartyszko@gmail.com>
2020-06-06 11:47:30 +0200

    First commit

---
+++ test_case1.py
@@ -0,0 +1,9 @@
+import unittest
+
+class Test(unittest.TestCase):
+    def test_firstTest(self):
+        print('This iis my first unit test case')
+
+
+if __name__ == '__main__':
+    unittest.main()
